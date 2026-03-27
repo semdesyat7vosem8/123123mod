677b867fcc063fb69827dd72f9b6e64d4097f463
semdesyat7vosem8 <164517885+semdesyat7vosem8@users.noreply.github.com>
2026-03-27 15:54:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
 
 let commandQueue = [];
